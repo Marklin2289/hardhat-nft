4bf6dc9ee811f75ec3c89f33b55e79ebb42d4f6a
marklin2289 <marklin2289@gmail.com>
2022-08-12 11:59:43 -0700

    edited config

--- hardhat.config.js
+++ hardhat.config.js
@@ -38,6 +38,9 @@ module.exports = {
             {
                 version: "0.6.6",
             },
+            {
+                version: "0.6.0",
+            },
         ],
     },
     etherscan: {
