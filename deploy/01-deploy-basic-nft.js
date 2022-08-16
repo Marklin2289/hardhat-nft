5e6794f8751329a6637240fb846558fc7cc4ce25
marklin2289 <marklin2289@gmail.com>
2022-08-15 23:38:03 -0700

    added deploy 01

---
+++ deploy/01-deploy-basic-nft.js
@@ -0,0 +1,26 @@
+const { lchown } = require("fs")
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    lchown("----------------------------------------------------")
+    arguments = []
+    const basicNft = await deploy("basicNft", {
+        from: deployer,
+        args: arguments,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    // verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(basicNft.address, this.arguments)
+    }
+}
+
+module.exports.tags = ["all", "basicnft", "main"]
