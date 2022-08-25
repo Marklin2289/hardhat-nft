9e689584b50dfac61fc0048d62743d6d9d3a69be
marklin2289 <marklin2289@gmail.com>
2022-08-24 20:22:31 -0700

    edited mintNft() in BasicNft.sol added return value

--- contracts/RandonIpfsNft.sol
+++ contracts/RandonIpfsNft.sol
@@ -7,7 +7,7 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "hardhat/console.sol";
 
-contract RandomIpfsNft {
+contract RandomIpfsNft is VRFConsumerBaseV2 {
     // when we mint and  NFT, we will trigger a Chainlink VRF to get us a random number
     //  using that number, we will get a random NFT
     // PUG, Shiba Inu, St. Bernard
@@ -17,9 +17,42 @@ contract RandomIpfsNft {
 
     // users have to pay to min an NFT
     // the owner of the contract can withdraw the ETH
-    function requestNft() public {}
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal {}
+    // Chainlink VRF Variables
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane,
+        // uint256 mintFee,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_subscriptionId = subscriptionId;
+        i_gasLane = gasLane;
+        i_callbackGasLimit = callbackGasLimit;
+    }
+
+    function requestNft() public returns (uint256 requestId) {
+        requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
 
     function tokenURI(uint256) public {}
 }
