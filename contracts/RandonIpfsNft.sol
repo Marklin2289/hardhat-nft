a1f49b7ba7945fe70b44d74139b20abfe517e0e8
marklin2289 <marklin2289@gmail.com>
2022-08-24 21:01:20 -0700

    import ERC721.sol & added on contract and constuctor ERC('NAME1','NAME2' for _safemint()

--- contracts/RandonIpfsNft.sol
+++ contracts/RandonIpfsNft.sol
@@ -1,13 +1,13 @@
 //  SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
+pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "hardhat/console.sol";
 
-contract RandomIpfsNft is VRFConsumerBaseV2 {
+contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
     // when we mint and  NFT, we will trigger a Chainlink VRF to get us a random number
     //  using that number, we will get a random NFT
     // PUG, Shiba Inu, St. Bernard
@@ -29,13 +29,16 @@ contract RandomIpfsNft is VRFConsumerBaseV2 {
     // VRF Helpers
     mapping(uint256 => address) public s_requestIdToSender;
 
+    // NFT variables
+    uint256 public s_tokenCounter; //should be private, public for learning purpose
+
     constructor(
         address vrfCoordinatorV2,
         uint64 subscriptionId,
         bytes32 gasLane,
         // uint256 mintFee,
         uint32 callbackGasLimit
-    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS NFT", "RIN") {
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_subscriptionId = subscriptionId;
         i_gasLane = gasLane;
@@ -57,8 +60,10 @@ contract RandomIpfsNft is VRFConsumerBaseV2 {
         // _safeMint(msg.sender, s_tokenCounter);
         // msg.sender is actually the chainlink node not the actual owner
         // therefore, we need to set a mapping connect the owner and requestId
-        address dogOwner = s_requestIdToSender[requestId];
+        address dogOwner = s_requestIdToSender[requestId]; // => msg.sender
+        uint256 newTokenId = s_tokenCounter; // => s_tokenCounter
+        _safeMint(dogOwner, newTokenId);
     }
 
-    function tokenURI(uint256) public {}
+    // function tokenURI(uint256) public override {}
 }
