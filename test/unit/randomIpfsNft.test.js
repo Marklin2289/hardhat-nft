870237d38834e0b72560288d7ec9c00f27b1ef3a
marklin2289 <marklin2289@gmail.com>
2022-09-05 23:15:22 -0700

    added test.js

--- test/unit/randomIpfsNft.test.js
+++ test/unit/randomIpfsNft.test.js
@@ -1,3 +1,4 @@
+const { assert } = require("chai")
 const { network, ethers, deployments } = require("hardhat")
 const { developmentChains } = require("../../helper-hardhat-config")
 
@@ -18,6 +19,41 @@ const { developmentChains } = require("../../helper-hardhat-config")
               it("sets starting values correctly", async () => {
                   const dogTokenUriZero = await randomIpfsNft.getDogTokenUris(0)
                   const isInitialized = await randomIpfsNft.getInitialized()
+                  assert(dogTokenUriZero.includes("ipfs://"))
+                  assert.equal(isInitialized, true)
+              })
+          })
+
+          describe("fulfillRandomWords", () => {
+              it("mint NFT after random number is returned", async () => {
+                  await new Promise(async (resolve, reject) => {
+                      randomIpfsNft.once("NftMinted", async () => {
+                          try {
+                              const tokenUri = await randomIpfsNft.tokenURI("0")
+                              const tokenCounter = await randomIpfsNft.getTokenCounter()
+                              assert.equal(tokenUri.toString().includes("ipfs://"), true)
+                              assert.equal(tokenCounter.toString(), "1")
+                              resolve()
+                          } catch (e) {
+                              console.log(e)
+                              reject(e)
+                          }
+                      })
+                      try {
+                          const fee = await randomIpfsNft.getMintFee()
+                          const requestNftResponse = await randomIpfsNft.requestNft({
+                              value: fee.toString(),
+                          })
+                          const requestNftReceipt = await requestNftResponse.wait(1)
+                          await vrfCoordinatorV2Mock.fulfillRandomWords(
+                              requestNftReceipt.events[1].args.requestId,
+                              randomIpfsNft.address
+                          )
+                      } catch (e) {
+                          console.log(e)
+                          reject(e)
+                      }
+                  })
               })
           })
       })
