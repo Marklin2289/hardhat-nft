5ff7a431c19b34f7c6b688588c29d39516b4d1d2
marklin2289 <marklin2289@gmail.com>
2022-09-06 23:25:57 -0700

    setup DynamicSvgNft.sol

---
+++ contracts/DynamicSvgNft.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "base64-sol/base64.sol";
+import "hardhat/console.sol";
