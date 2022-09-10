7b2e6550c46d44d7b034130988e22be09d13bc3d
marklin2289 <marklin2289@gmail.com>
2022-09-09 20:26:40 -0700

    yarn add --dev @chainlink/contracts and added tokenURI()

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -8,6 +8,8 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "base64-sol/base64.sol";
 import "hardhat/console.sol";
 
+// error ERC721Metadata_URI_QueryFor_NonExistentToken();
+
 contract DynamicSvgNft is ERC721, Ownable {
     /* 
     mint
@@ -34,13 +36,44 @@ contract DynamicSvgNft is ERC721, Ownable {
     }
 
     function svgToImageURI(string memory svg) public pure returns (string memory) {
-        string memory baseURL = "data:image/svg+xml;base64,";
+        // string memory baseURL = "data:image/svg+xml;base64,";
         string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
-        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+        return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
     }
 
     function mintNft()public {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter += 1;
+    }
+
+    function _baseURI() internal pure override returns(string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns(string memory) {
+        // if(!_exists(tokenId)) {
+        //     revert ERC721Metadata_URI_QueryFor_NonExistentToken();
+        // }
+        require(_exists(tokenId), "URI Query for nonexistent token");
+        string memory imageURI = "Hi";
+
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(), // You can add whatever name here
+                                '", "description":"An NFT that changes based on the Chainlink Feed", ',
+                                '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
     }
 }
