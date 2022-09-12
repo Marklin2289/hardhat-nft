bde4f3318429063c0aa24eb456771873f516a050
marklin2289 <marklin2289@gmail.com>
2022-09-12 02:24:08 -0700

    deployed on testnets

--- deploy/03-deploy-dynamic-svg-nft.js
+++ deploy/03-deploy-dynamic-svg-nft.js
@@ -21,10 +21,10 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const highSVG = fs.readFileSync("./images/dynamicNft/happy.svg", { encoding: "utf8" })
 
     log("----------------------------------------------------")
-    arguments = [ethUsdPriceFeedAddress, lowSVG, highSVG]
+    args = [ethUsdPriceFeedAddress, lowSVG, highSVG]
     const dynamicSvgNft = await deploy("DynamicSvgNft", {
         from: deployer,
-        args: arguments,
+        args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
@@ -32,7 +32,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     // Verify the deployment
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         log("Verifying...")
-        await verify(dynamicSvgNft.address, arguments)
+        await verify(dynamicSvgNft.address, args)
     }
 }
 
