ae42a9a587dab18a55ffef9fc41401a5700c991f
marklin2289 <marklin2289@gmail.com>
2022-09-11 22:22:43 -0700

    added 04-mint.js and hh deploy

--- deploy/04-mint.js
+++ deploy/04-mint.js
@@ -8,6 +8,7 @@ module.exports = async ({ getNamedAccounts }) => {
     const basicNft = await ethers.getContract("BasicNft", deployer)
     const basicMinTx = await basicNft.mintNft()
     await basicMinTx.wait(1)
+    console.log("---------------------------------------------------------------------")
     console.log(`Basic NFT index 0 tokenURI is : ${await basicNft.tokenURI(0)}`)
 
     // Dynamic SVG NFT mint
@@ -15,5 +16,29 @@ module.exports = async ({ getNamedAccounts }) => {
     const dynamicSvgNft = await ethers.getContract("DynamicSvgNft", deployer)
     const dynamicSvgNftMinTx = await dynamicSvgNft.mintNft(highValue)
     await dynamicSvgNftMinTx.wait(1)
-    console.log(`Dynamic SVG NFT index 0 tokenURI is ${dynamicSvgNft.tokenURI(0)}`)
+    console.log("---------------------------------------------------------------------")
+    console.log(`Dynamic SVG NFT index 0 tokenURI is ${await dynamicSvgNft.tokenURI(0)}`)
+
+    // random IPFS NFT mint
+    const randomIpfsNft = await ethers.getContract("RandomIpfsNft", deployer)
+    const mintFee = await randomIpfsNft.getMintFee()
+    const randomIpfsNftMinTx = await randomIpfsNft.requestNft({ value: mintFee.toString() })
+    const randomIpfsNftMintTxReceipt = await randomIpfsNftMinTx.wait(1)
+    //  Need to listen for response
+    await new Promise(async (resolve, reject) => {
+        setTimeout(() => reject("Timeout: 'NFTMinted' event did not fire"), 300000) // 5 minutes timeout
+        // setup listener for our event
+        randomIpfsNft.once("NftMinted", async () => {
+            resolve()
+        })
+        if (chainId == 31337) {
+            const requestId = randomIpfsNftMintTxReceipt.events[1].args.requestId.toString()
+            const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+            await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, randomIpfsNft.address)
+        }
+    })
+    console.log("---------------------------------------------------------------------")
+    console.log(`Random IPFS NFT index 0 tokenURI is : ${await randomIpfsNft.tokenURI(0)}`)
 }
+
+module.exports.tags = ["all", "mint"]
